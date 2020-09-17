6d45b3a2ed90733c170f510cec1f596cbaf55965
Mirudistok <mirudistok32@yandex.ru>
2020-09-17 10:47:54 +0300

    Create Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { PATH } from './Route'
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Switch>
+        <Route exact path={PATH.empty} render={() => <Redirect to={PATH.home} />} />
+        <Route exact path={PATH.home} render={() => <div>Home</div>}/>
+        <Route render={() => <div>Error</div>} />
+      </Switch>
     </div>
   );
 }
