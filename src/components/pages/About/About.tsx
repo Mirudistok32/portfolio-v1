e224904cc4b20282ef717e8ceb974c9e0a3f43a8
Mirudistok <mirudistok32@yandex.ru>
2020-09-19 15:21:36 +0300

    Create component Subtitle

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './About.module.scss'
 import { Title } from '../../Title/Title'
+import { Subtitle } from '../../Subtitle/Subtitle'
 
 type PropsType = {
 
@@ -12,9 +13,11 @@ export const About: React.FC<PropsType> = React.memo((props) => {
         <div className={s.about}>
             <div className={s.about__wrap}>
                 <div className={s.about__title}>
-                    <Title title={'About Me'}/>
+                    <Title title={'About Me'} />
                 </div>
-                <div className={s.about__subtitle}></div>
+                <div className={s.about__subtitle}>
+                    <Subtitle subtitle={'Today you make the code, tomorrow the code makes you money'} />
+                </div>
                 <div className={s['about__info-wrap']}>
                     <div className={s['about__info-photo']}></div>
                     <div className={s['about__info-data']}>
