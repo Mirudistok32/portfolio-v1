8459ce7cfbf79e8714aeaedd67cb5febc3c7b51c
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-21 22:12:25 +0300

    Updates

---
+++ precache-manifest.416390831a17e2f1a9483329cad816fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c815fde3bd1614823cf3f5c4dd9b8e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5b6e0a9467434848e866",
+    "url": "/portfolio/static/css/main.151a52e5.chunk.css"
+  },
+  {
+    "revision": "9dcc3cd240414d23c3ff",
+    "url": "/portfolio/static/js/2.a25f2de7.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/portfolio/static/js/2.a25f2de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6e0a9467434848e866",
+    "url": "/portfolio/static/js/main.217be20a.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "7bb131b2775263c1a1dcfcf8515a8da4",
+    "url": "/portfolio/static/media/forest-5286824.7bb131b2.jpg"
+  },
+  {
+    "revision": "384a8094e32145b56bf38a473c72b78b",
+    "url": "/portfolio/static/media/photoForAvatar.384a8094.png"
+  }
+]);
