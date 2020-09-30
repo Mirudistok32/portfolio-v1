1f5dd1b8ca21990ee60de345f357d4b06981fe72
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-30 17:20:52 +0300

    Delete fakes files and Strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </HashRouter>
-  </React.StrictMode>,
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
